8d1c92a129f5e59866d87209695ff522e1df1ce5
Grisha <odnokonnygrisha@gmail.com>
2023-06-23 12:57:22 +0300

    feat second Task in process

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int i = 0;
+while (number > 0)
+{
+    int ostatok = number % 10;
+    i += ostatok;
+}
+int result = number + i;
+
+Consol.eWriteLine(result);
